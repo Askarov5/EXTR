fb2be4f1a67aecb9f873db94bb37c2096cbc1b7e
Askarov5 <oroz.askarov@gmail.com>
2018-06-04 05:38:20 +0300

    Project done!

--- src/js/app.js
+++ src/js/app.js
@@ -1,5 +1,7 @@
-import hello from './hello';
+import 'slick-carousel';
+import PerfectScrollbar from 'perfect-scrollbar';
 $(document).ready(function(){
+    var winWidth = $(window).width();
     $('.notifications-item__head').click(function() {
         $(this).toggleClass('active');
         $(this).parent().toggleClass('active');
@@ -16,4 +18,27 @@ $(document).ready(function(){
         $(this).toggleClass('active');
         $(this).next().toggleClass('active');
     });
+    scrollBarSwitcher();
+    slickSwitcher();
 });
+$(window).on('resize', function() {
+    scrollBarSwitcher();
+    slickSwitcher();
+});
+
+function slickSwitcher() {
+    if($(window).width() <= 640) {
+        $('.bloglist').slick({
+            dots: true,
+            arrows: false,
+            infinite: false,
+        });
+        ps.destroy;
+    }
+}
+
+function scrollBarSwitcher() {
+    if($(window).width() > 640) {
+        var ps = new PerfectScrollbar('.bloglist');
+    }
+}
